7d7c7d17c5c049851f9a0159d3da0f8ed1f56b63
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-27 01:55:32 +0900

    [add]ダメージを受ける機能を持つインターフェースを追加

---
+++ Assets/Scripts/Interfaces/IDamagable.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// ダメージを受ける機能のインターフェース
+/// </summary>
+public interface IDamagable
+{
+    #region property
+    /// <summary>現在のHP</summary>
+    int CurrentHp { get; }
+
+    /// <summary>無敵状態かどうか</summary>
+    bool IsInvincible { get; }
+    #endregion
+
+    #region public method
+    /// <summary>
+    /// ダメージを受ける
+    /// </summary>
+    /// <param name="amount">ダメージ量</param>
+    void Damage(int amount);
+    #endregion
+}
