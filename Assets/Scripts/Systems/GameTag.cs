80e99b941767ba4f437e15ae94eef31f50374344
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-27 01:54:44 +0900

    [add]ゲーム中のタグをまとめたクラスを追加

---
+++ Assets/Scripts/Systems/GameTag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲーム内で使用するタグを管理するクラス
+/// </summary>
+public class GameTag
+{
+    #region property
+    public static string Player => _player;
+    public static string Enemy => _enemy;
+    #endregion
+
+    #region Constant
+    /// <summary>
+    /// プレイヤー
+    /// </summary>
+    private static string _player = "Player";
+    /// <summary>
+    /// 敵
+    /// </summary>
+    private static string _enemy = "Enemy";
+    #endregion
+}
