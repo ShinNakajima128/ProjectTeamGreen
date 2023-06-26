014a381d90058964ccb936cf23e81deadb4035d7
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-27 01:57:07 +0900

    [add]敵の機能のベースクラスを仮作成

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,13 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
-public class EnemyBase : MonoBehaviour
+/// <summary>
+/// 敵の機能全般を管理するベースクラス。
+/// このクラスは直接アタッチできないので、必ず継承して敵を作成してください。
+/// </summary>
+public abstract class EnemyBase : MonoBehaviour, IDamagable
 {
     #region property
+    public int CurrentHp => _currentHp;
+
+    public bool IsInvincible => _isInvincible;
     #endregion
 
     #region serialize
+    [SerializeField]
+    private EnemyData _enemyData = default;
+    #endregion
+
+    #region protected
+    /// <summary>
+    /// 現在のHP。実行中はこの変数を扱う 
+    /// </summary>
+    protected int _currentHp;
+    /// <summary>
+    /// 現在の攻撃力。実行中はこの変数を扱う 
+    /// </summary>
+    protected int _currentAttackAmount;
+    protected bool _isActived = false;
+    protected bool _isInvincible = false;
+   
     #endregion
 
     #region private
@@ -20,19 +44,37 @@ public class EnemyBase : MonoBehaviour
     #endregion
 
     #region unity methods
-    private void Awake()
+    protected virtual void Awake()
+    {
+        _currentHp = _enemyData.HP;
+        _currentAttackAmount = _enemyData.AttackAmount;
+    }
+
+    protected virtual void Start()
     {
 
     }
 
-    private void Start()
+    protected virtual void OnTriggerEnter(Collider other)
     {
-
+        //プレイヤーにヒットした場合
+        if (other.CompareTag(GameTag.Player))
+        {
+            //インターフェースを通じてダメージ処理を実行
+            if (TryGetComponent(out IDamagable target))
+            {
+                //体力が0ではない且つ無敵状態ではない場合はダメージを与える
+                if (target.CurrentHp > 0 && !target.IsInvincible)
+                {
+                    target.Damage(_currentAttackAmount);
+                }
+            }
+        }
     }
 
-    private void Update()
+    public void Damage(int amount)
     {
-
+        throw new System.NotImplementedException();
     }
     #endregion
 
