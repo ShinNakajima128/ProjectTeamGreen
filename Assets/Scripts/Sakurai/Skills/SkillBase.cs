4684ea98e20a101eefa6123f18984c4b497a9457
Sakurai8080 <eccion8080@gmai.com>
2023-06-27 18:33:30 +0900

    [update]スキルデータとスキルベースのスクリプトを追加。

---
+++ Assets/Scripts/Sakurai/Skills/SkillBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBase : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+private void Awake()
+{
+
+}
+
+private void Start()
+{
+
+}
+
+private void Update()
+{
+
+}
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+
+    
+    #endregion
+}
