1e08f80c7b8c3f3ccb036a68235bb42caec5edc2
takehitoobaru <obaru.namu1101@gmail.com>
2023-06-27 18:32:26 +0900

    [add]アイテムデータ作成、スクリプトの追加

---
+++ Assets/Scripts/Obaru/ItemBase.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemBase : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    [Tooltip("アイテムデータ")]
+    [SerializeField]
+    private ItemData _itemData = default;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region protected method
+    protected abstract void ItemUse();
+    #endregion
+
+    #region private method
+    #endregion
+}
