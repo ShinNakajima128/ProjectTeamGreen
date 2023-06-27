bc6323da62b3e930d285f25cc1310f6575647401
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-27 13:57:44 +0900

    [update]フォルダの整頓

---
+++ Assets/Scripts/Nakajima/Players/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IDamagable
+{
+    
+    
+    #region property
+    public bool IsInvincible => throw new System.NotImplementedException();
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+
+    #endregion
+
+    #region public method
+    public void Damage(int amount)
+    {
+        Debug.Log("Playerがダメージを受けた");
+    }
+    #endregion
+
+    #region private method
+    #endregion
+}
