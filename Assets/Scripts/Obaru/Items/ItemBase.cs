30fd004c12828872d1047d11c929072fda970397
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-28 15:51:17 +0900

    [update]Itemの機能を更新

--- Assets/Scripts/Obaru/Items/ItemBase.cs
+++ Assets/Scripts/Obaru/Items/ItemBase.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 各アイテムのベースクラス
+/// </summary>
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public abstract class ItemBase : MonoBehaviour
 {
     #region property
+    public ItemType ItemType => _itemData.ItemType;
     #endregion
 
     #region serialize
@@ -32,18 +38,20 @@ public abstract class ItemBase : MonoBehaviour
     {
 
     }
-
-    private void Update()
-    {
-
-    }
     #endregion
 
     #region public method
     #endregion
 
     #region protected method
-    protected abstract void ItemUse();
+    /// <summary>
+    /// 使用する
+    /// </summary>
+    public abstract void Use(PlayerController player);
+    /// <summary>
+    /// ステージ上から無くなる
+    /// </summary>
+    public abstract void Return();
     #endregion
 
     #region private method
