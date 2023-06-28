f11aa46b3c13a04b3b97140a0e686d98fc91b0d2
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-28 15:58:51 +0900

    [add]テスト用の仮アイテムの作成と、アイテムの生成機能を追加

---
+++ Assets/Scripts/Nakajima/Test/TempItem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// テスト用の仮アイテムのコンポーネント
+/// </summary>
+public class TempItem : ItemBase
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    /// <summary>
+    /// アイテムを使用する
+    /// </summary>
+    /// <param name="player"></param>
+    public override void Use(PlayerController player)
+    {
+        Debug.Log($"{ItemType}を使用した");
+        Destroy(gameObject);
+
+        //HPを回復する処理は、以下のように記述する
+        //player.Heal(回復量);
+
+        //他のステータスを上げる処理は、まだステータス側の機能ができていないため
+        //まだ記述できませんが、近日中にステータスの機能を作成する予定です
+    }
+
+    /// <summary>
+    /// 非アクティブ状態にする。ゲーム終了時など、不要となった時に実行される
+    /// </summary>
+    public override void Return()
+    {
+        Debug.Log($"{ItemType}を非アクティブにした");
+
+        //座標をリセットして非表示にする
+        gameObject.transform.localPosition = Vector2.zero;
+        gameObject.SetActive(false);
+    }
+    #endregion
+
+    #region private method
+    #endregion
+}
---
+++ Assets/Scripts/Obaru/Items/ItemGenerator.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムの生成、破棄をする機能を持つコンポーネント
+/// </summary>
+public class ItemGenerator : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    [Tooltip("次に生成するまでの間隔")]
+    [SerializeField]
+    private float _generateInterval = 1.0f;
+
+    [Tooltip("各アイテム")]
+    [SerializeField]
+    private ItemBase[] _items = default;
+    #endregion
+
+    #region private
+    private Transform _playerTrans;
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+        _playerTrans = GameObject.FindGameObjectWithTag(GameTag.Player).transform;
+    }
+
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    /// <summary>
+    /// 生成する
+    /// </summary>
+    /// <param name="type">アイテムの種類</param>
+    /// <param name="pos">生成する位置</param>
+    public void Generate(ItemType type, Vector2 pos)
+    {
+        //指定されたアイテムのオブジェクトデータを取得
+        var item = _items.FirstOrDefault(x => x.ItemType == type);
+
+        //指定した座標に生成。現状は仮の処理で、プーリングしたオブジェクトを使用する処理に修正予定
+        Instantiate(item, pos, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// ランダムな位置にアイテムを生成する
+    /// </summary>
+    public void RandoomGenerate()
+    {
+        StartCoroutine(RandomGenerateCoroutine());
+    }
+    #endregion
+
+    #region private method
+    #endregion
+
+    #region coroutine method
+    /// <summary>
+    /// ランダムな位置にアイテムを生成するコルーチン
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator RandomGenerateCoroutine()
+    {
+        while (true)
+        {
+            float randomX = Random.Range(_playerTrans.position.x + 2, _playerTrans.position.x + 3.5f);
+            float randomY = Random.Range(_playerTrans.position.y + 2, _playerTrans.position.y + 3.5f);
+
+            randomX = Random.Range(0, 2) == 0 ? randomX : randomX * -1;
+            randomY = Random.Range(0, 2) == 0 ? randomY : randomY * -1;
+
+            Vector2 generatePos = new Vector2(randomX, randomY);
+
+            //テストとして、現状は回復アイテムのみを生成する
+            Generate(ItemType.Heal, generatePos);
+            yield return new WaitForSeconds(_generateInterval);
+        }
+    }
+    #endregion
+}
