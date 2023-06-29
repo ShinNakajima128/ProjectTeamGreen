6c0791ca5d1c06eaa7404db4386b657700d6ce31
takehitoobaru <obaru.namu1101@gmail.com>
2023-06-29 10:32:50 +0900

    [add]Testフォルダ、ItemTestスクリプトを追加

---
+++ Assets/Scripts/Obaru/Test/ItemTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class ItemTest : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    [SerializeField]
+    PlayerController _player;
+
+    [SerializeField]
+    PlayerHealth _health;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                Debug.Log($"HP:{_health.CurrentHP}");
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    _player.Damage(10);
+                }
+            })
+            .AddTo(this);
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
