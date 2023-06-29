a042ba9f65bb19c44f5120b8da2828e196ce4486
Sakurai8080 <eccion8080@gmai.com>
2023-06-29 12:13:00 +0900

    [add]スキルテストに使用するためのSkillTestクラスを作成しました。

---
+++ Assets/Scripts/Sakurai/Skills/SkillTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class SkillTest : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        //Update内で行う処理を登録
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SkillManager.Instance.SetSkill(SkillType.Aura);
+                }
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    SkillManager.Instance.SetSkill(SkillType.Fairy);
+                }
+            })
+            .AddTo(this);
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
