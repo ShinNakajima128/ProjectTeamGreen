7019eacfc252b32e6987a4b02fffb98c918f6639
takehitoobaru <obaru.namu1101@gmail.com>
2023-06-30 10:36:11 +0900

    [add]Skillフォルダ作成。MuscleThrowSkillスクリプトとDumbbellスクリプト追加。

---
+++ Assets/Scripts/Obaru/Skills/MuscleThrowSkill.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuscleThrowSkill : SkillBase
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    public override void LebelUpSkill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSkillAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AttackUpSkill(float coefficient)
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+
+    #region private method
+    #endregion
+
+    #region coroutine method
+    protected override IEnumerator SkillActionCoroutine()
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+}
