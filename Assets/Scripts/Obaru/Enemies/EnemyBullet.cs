a0260ad2da3825d7a023f352690db92d9db35c29
takehitoobaru <obaru.namu1101@gmail.com>
2023-07-03 16:19:46 +0900

    [add]EnemyBulletスクリプトを作成

---
+++ Assets/Scripts/Obaru/Enemies/EnemyBullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    [Tooltip("弾の速さ")]
+    [SerializeField]
+    private float _moveSpeed = 3.0f;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
