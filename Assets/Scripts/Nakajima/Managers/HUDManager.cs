c1bd525f818f2a54a25bafe2c018c39e8070ea23
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-07-03 12:10:49 +0900

    [add]HUD上に討伐数を表示するTextを追加

---
+++ Assets/Scripts/Nakajima/Managers/HUDManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// HUDの機能全般を管理するManagerクラス
+/// </summary>
+public class HUDManager : MonoBehaviour
+{
+    #region property
+    public static HUDManager Instance { get; private set; }
+    #endregion
+
+    #region serialize
+    [Tooltip("討伐数を表示するText")]
+    [SerializeField]
+    private Text _defeatText = default;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        //討伐数が変化した時の処理を登録
+        EnemyManager.Instance.DefeatedEnemyAmountViewObserver
+                             .Subscribe(value => ViewDefeatedAmount(value))
+                             .AddTo(this);
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    /// <summary>
+    /// 討伐数を表示する
+    /// </summary>
+    /// <param name="amount">討伐数</param>
+    private void ViewDefeatedAmount(uint amount)
+    {
+        _defeatText.text = $"討伐数:{amount}体";
+    }
+    #endregion
+}
