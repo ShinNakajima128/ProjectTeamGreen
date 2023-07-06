abfed4beca00c1febdc7f02ecb997917ead6d14e
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-07-06 18:54:45 +0900

    [add]制限時間を管理するManagerクラスを追加

---
+++ Assets/Scripts/Nakajima/Managers/TimeManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// 時間関連の機能を管理するManagerクラス
+/// </summary>
+public class TimeManager : MonoBehaviour
+{
+    #region property
+    public static TimeManager Instance { get; private set; }
+    #endregion
+
+    #region serialize
+    [Tooltip("制限時間(分)")]
+    [SerializeField]
+    private uint _limitTime = 15;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
