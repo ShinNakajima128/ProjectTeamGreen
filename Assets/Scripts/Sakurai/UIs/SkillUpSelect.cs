735c739a3cca8b5f08d75f38608d62cbf1704617
Sakurai8080 <eccion8080@gmai.com>
2023-07-07 14:13:28 +0900

    [update]アイテムプレハブのオーダーインレイヤー変更

--- Assets/Scripts/Sakurai/UIs/SkillUpSelect.cs
+++ Assets/Scripts/Sakurai/UIs/SkillUpSelect.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using UniRx;
 
+/// <summary>
+/// スキルアップ時のUI管理
+/// </summary>
 public class SkillUpSelect : MonoBehaviour
 {
     #region property
+    
     #endregion
 
     #region serialize
     [Header("変数")]
     [Tooltip("スキルセレクトUIを格納するリスト")]
     [SerializeField]
-    List<Button> _skillSelectUis = default;
+    List<Button> _skillSelectUIs = default;
 
-    [Tooltip("経験値の値を取得するための参照")]
-    [SerializeField]
-    PlayerStatus playerStatus = default; 
     #endregion
 
     #region private
@@ -27,51 +29,41 @@ public class SkillUpSelect : MonoBehaviour
 
     #region Constant
     #endregion
-
+    
     #region Event
     #endregion
 
     #region unity methods
-    private void Awake()
-    {
-       
-    }
-
     private void Start()
     {
-        if (playerStatus.CurrentPlayerLevel.Value == 1)
-        {
-            ActivateRondomSkillUIs();
-        }
-        for (int i = 0; i < _skillSelectUis.Count; i++)
+        PlayerController.Instance.Status.CurrentPlayerLevel
+                                        .Skip(1)
+                                        .Subscribe(_ => ActivateRondomSkillUIs())
+                                        .AddTo(this);
+        
+        for (int i = 0; i < _skillSelectUIs.Count; i++)
         {
             SkillType type = (SkillType)i;
-            _skillSelectUis[i].onClick.AddListener(() => OnSkill(type));
+            _skillSelectUIs[i].onClick.AddListener(() => OnSkill(type));
         }
-    }
-
-    private void Update()
-    {
-        //if (playerStatus.CurrentRequireExp.Value <= playerStatus.CurrentExp.Value)
-        //{
-        //    ActivateRondomSkillUIs();
-        //}
-
     }
     #endregion
 
     #region public method
 
+    /// <summary>
+    /// スキルアップした時にUIをランダムで表示させる
+    /// </summary>
     public void ActivateRondomSkillUIs()
     {
 
-        IEnumerable randomIndices = Enumerable.Range(0, _skillSelectUis.Count)
+        IEnumerable randomIndices = Enumerable.Range(0, _skillSelectUIs.Count)
                                     .OrderBy(x => Random.value)
                                     .Take(_activeAmount);
 
         foreach (int index in randomIndices)
         {
-            _skillSelectUis[index].gameObject.SetActive(true);
+            _skillSelectUIs[index].gameObject.SetActive(true);
         }
 
         Time.timeScale = 0f;
@@ -80,11 +72,15 @@ public class SkillUpSelect : MonoBehaviour
 
     #region private method
 
+    /// <summary>
+    /// OnClickしたときの処理
+    /// </summary>
+    /// <param name="type">各ボタンのスキルタイプ</param>
     private void OnSkill(SkillType type)
     {
         SkillManager.Instance.SetSkill(type);
 
-        foreach (Button skillUI in _skillSelectUis)
+        foreach (Button skillUI in _skillSelectUIs)
         {
             skillUI.gameObject.SetActive(false); 
         }
