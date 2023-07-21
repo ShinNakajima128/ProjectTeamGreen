1382e977916bda746c95d3eb5234b9320487e096
takehitoobaru <obaru.namu1101@gmail.com>
2023-07-21 17:29:57 +0900

    [add]PotatoをRockに変更

---
+++ Assets/Scripts/Obaru/Enemies/RockGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rockの生成
+/// </summary>
+public class RockGenerator : MonoBehaviour
+{
+    #region property
+    public ObjectPool<Rock> RockPool => _rockPool;
+    #endregion
+
+    #region serialize
+    [Tooltip("プレハブ")]
+    [SerializeField]
+    private Rock _rockPrefab = default;
+
+    [Tooltip("親")]
+    [SerializeField]
+    private Transform _parent = default;
+    #endregion
+
+    #region private
+    private ObjectPool<Rock> _rockPool;
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+        _rockPool = new ObjectPool<Rock>(_rockPrefab, _parent);
+    }
+    #endregion
+}
