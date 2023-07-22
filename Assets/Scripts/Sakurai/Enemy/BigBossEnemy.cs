fbd9db92800136a51dedc2945d3b7e65166d419d
Sakurai8080 <sakuraitakuya8080@gmail.com>
2023-07-22 23:06:27 +0900

    [update]BigBossEnemyクラスのリファクタリングをしました。

--- Assets/Scripts/Sakurai/Enemy/BigBossEnemy.cs
+++ Assets/Scripts/Sakurai/Enemy/BigBossEnemy.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
+/// <summary>
+/// ラスボスの機能
+/// </summary>
 public class BigBossEnemy : BossEnemyBase
 {
-    #region property
-    #endregion
-
     #region serialize
     [Header("変数")]
     [Tooltip("移動速度")]
@@ -32,22 +31,18 @@ public class BigBossEnemy : BossEnemyBase
     #endregion
 
     #region private
-
+    /// <summary>コルーチンの待機時間</summary>
     private float _waitTime = 1.0f;
 
+    /// <summary>プレイヤーの位置の応じて反転</summary>
     private bool _isFliped = false;
+
+    /// <summary>攻撃時のバレットの数</summary>
+    private float _bulletCount = 7.0f;
 
     private Rigidbody2D _rd2D;
 
     private Collider2D _col2D;
-
-    private float _bulletCount = 7.0f;
-    #endregion
-
-    #region Constant
-    #endregion
-
-    #region Event
     #endregion
 
     #region unity methods
@@ -73,17 +68,12 @@ public class BigBossEnemy : BossEnemyBase
     {
         base.OnDisable();
     }
-
-    private void Update()
-    {
-        Debug.Log(_rd2D.velocity.y);
-    }
-    #endregion
-
-    #region public method
     #endregion
 
     #region private method
+    /// <summary>
+    /// プレイヤーの向きに応じて反転
+    /// </summary>
     private void EnemyFrip()
     {
         if (!_isFliped && transform.localPosition.x < _playerTrans.position.x)
@@ -99,21 +89,26 @@ public class BigBossEnemy : BossEnemyBase
     }
     #endregion
 
+    /// <summary>
+    /// 大ボスのステートマシン
+    /// </summary>
     protected override IEnumerator OnActionCoroutine()
     {
         while (true)
         {
             switch (_currentState)
             {
+                //アイドルステート
                 case BossState.Idle:
                     _waitTime = 3.0f;
                     yield return new WaitForSeconds(_waitTime);
                     _currentState = BossState.FirstMove;
                     break;
+                //突進ステート
                 case BossState.FirstMove:
                     yield return StartCoroutine(OnMoveCoroutine());
                     break;
-                //攻撃ステート
+                //攻撃ステート1
                 case BossState.FirstAttack:
                     yield return StartCoroutine(OnFirstAttackCoroutine());
                     break;
@@ -121,10 +116,12 @@ public class BigBossEnemy : BossEnemyBase
                 case BossState.SecondMove:
                     yield return StartCoroutine(OnJumpCoroutine());
                     break;
+                //攻撃ステート2
                 case BossState.SecondAttack:
                     yield return StartCoroutine(OnSecoundAttackCoroutine());
                     break;
                 default:
+                    Debug.Log($"{_currentState}はありません");
                     break;
             }
         }
@@ -133,13 +130,14 @@ public class BigBossEnemy : BossEnemyBase
     /// <summary>
     /// プレイヤーに突進する。
     /// </summary>
-    /// <returns></returns>
     private IEnumerator OnMoveCoroutine()
     {
         _waitTime = 2.0f;
 
+        //プレイヤーの向きに応じてエネミーの向き変更
         EnemyFrip();
 
+        //プレイヤーの位置
         Vector2 targetPos = _playerTrans.position;
 
         //プレイヤーとの距離が0.01以上であれば追いかける。
@@ -153,31 +151,37 @@ public class BigBossEnemy : BossEnemyBase
         _currentState = BossState.FirstAttack;
     }
 
+    /// <summary>
+    /// 1度目の攻撃
+    /// </summary>
     private IEnumerator OnFirstAttackCoroutine()
     {
         _waitTime = 0.2f;
+        
+        //プレイヤーの向きに応じてエネミーの向き変更
         EnemyFrip();
 
         _bulletCount = 7.0f;
-        
+
+        //弾を7方向に飛ばすため弾のカウントで割る
         float angleStep = 360.0f / _bulletCount;
 
         for (int i = 0; i < _bulletCount; i++)
         {
+            //プールから弾を取り出す
             BigBossBullet bulletobj = _generator.BigBossBulletPool.Rent();
 
             if (bulletobj != null)
             {
                 bulletobj.gameObject.SetActive(true);
-
                 bulletobj.transform.position = transform.position;
-
                 bulletobj.SetAttackAmount(_bulletAttackAmount);
 
+                //円形に均等に飛ばすため計算
                 float angle = i * angleStep;
 
+                //度をラジアンに変換して角度を求める。
                 Vector2 direction = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
-
                 bulletobj.SetVelocity(direction);
             }
             yield return new WaitForSeconds(_waitTime);
@@ -185,43 +189,60 @@ public class BigBossEnemy : BossEnemyBase
         }
     }
 
+    /// <summary>
+    /// プレイヤーの位置にジャンプ
+    /// </summary>
     private IEnumerator OnJumpCoroutine()
     {
         _waitTime = 3f;
         yield return new WaitForSeconds(_waitTime);
-
+        
         Vector2 targetPos = _playerTrans.position;
         float jumpForce = 8.0f;
+
+        //照準定める表現
         _targetPrefab.gameObject.SetActive(true);
         _targetPrefab.transform.position = targetPos;
+
+        //0.3秒,上方向に力を加える
         _rd2D.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+
+        //ジャンプ中はコライダーをきる
         _col2D.enabled = false;
         _waitTime = 0.3f;
         yield return new WaitForSeconds(_waitTime);
+
+        //速度を戻す
         _rd2D.velocity = new Vector2(_rd2D.velocity.x, 0);
 
+        //プレイヤーの位置に向かって落ちる
         while (Vector2.Distance(transform.localPosition,targetPos)>0.01f)
         {
             transform.localPosition = Vector2.MoveTowards(transform.localPosition, targetPos, _moveSpeed * Time.deltaTime);
             yield return null;
         }
+        //着地とどうじにコライダーを戻して煙を出して照準はきる。
+        _col2D.enabled = true;
         _smokePrefab.transform.position = transform.position;
         _smokePrefab.SetActive(true);
-        _col2D.enabled = true;
         _targetPrefab.gameObject.SetActive(false);
 
         _waitTime = 0.6f;
         yield return new WaitForSeconds(_waitTime);
-
         _smokePrefab.SetActive(false);
         _currentState = BossState.SecondAttack;
     }
 
+    /// <summary>
+    /// 2度目の攻撃
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator OnSecoundAttackCoroutine()
     {
         _waitTime = 0.2f;
         EnemyFrip();
 
+        //弾の数は10個
         _bulletCount = 10.0f;
 
         for (int i = 0; i < _bulletCount; i++)
@@ -233,11 +254,10 @@ public class BigBossEnemy : BossEnemyBase
                 if (bulletobj != null)
                 {
                     bulletobj.gameObject.SetActive(true);
-
                     bulletobj.transform.position = transform.position;
-
                     bulletobj.SetAttackAmount(_bulletAttackAmount);
 
+                    //プレイヤーに向かって10発連射。
                     bulletobj.SetVelocity((_playerTrans.position - transform.position).normalized);
 
                     yield return new WaitForSeconds(_waitTime);
